f73821a149f8a2bf1c8d7f4ed68fc06dd85f0c59
ZhienCloud <woonzhien1@gmail.com>
2023-04-10 23:07:42 +0800

    updated

--- app.js
+++ app.js
@@ -1,89 +1,126 @@
 $(() => {
-const audio = new Audio("Music.mp3");
-audio.play();});
+  let gameStarted = false;
+  $(document).ready(() => {
+    $("#popup").show();
+    $("#main").hide();
+    
+    
 
-$(() => {
+
+    $("#start-game").on("click", () => {
+      console.log("button");
+      $("#popup").hide();
+      $("#main").show();
+
+      const audio = new Audio("Music.mp3");
+
+      audio.play();
+      gameStarted = true;
+    });
+  });
+
+  //car positioning for blue,yellow,pink and green
+
   var counter = 0;
   const $bluecar = $("#blue-car");
+  const $redcar = $("#red-car");
   const $greencar = $("#green-car");
   const $pinkcar = $("#pink-car");
   const $yellowcar = $("#yellow-car");
-  // let carposition = [-180, -60, 60, 180];
-  // let random = carposition[(Math.floor(Math.random() * bluecarposition.length))];
+  
   $bluecar.on("animationiteration", () => {
-    let bluecarposition = [-180, -60, 60, 180];
-    let random = bluecarposition[(Math.floor(Math.random() * bluecarposition.length))];
-    $bluecar.css("left", random + "px");
+    let bluecarposition = [-23, -8, 8, 23];
+    let random =
+      bluecarposition[Math.floor(Math.random() * bluecarposition.length)];
+    $bluecar.css("left", random + "%");
     counter++;
-  })
+  });
   $greencar.on("animationiteration", () => {
-    let greencarposition = [-180, -60, 60, 180];
-    var random = greencarposition[(Math.floor(Math.random() * greencarposition.length))];
-    $greencar.css("left", random + "px");
+    let greencarposition = [-23, -8, 8, 23];
+    var random =
+      greencarposition[Math.floor(Math.random() * greencarposition.length)];
+    $greencar.css("left", random + "%");
     counter++;
-  })
+  });
   $pinkcar.on("animationiteration", () => {
-    let pinkcarposition = [-180, -60, 60, 180];
-    var random = pinkcarposition[(Math.floor(Math.random() * pinkcarposition.length))];
-    $pinkcar.css("left", random + "px");
+    let pinkcarposition = [-23, -8, 8, 23];
+    var random =
+      pinkcarposition[Math.floor(Math.random() * pinkcarposition.length)];
+    $pinkcar.css("left", random + "%");
     counter++;
-  })
+  });
   $yellowcar.on("animationiteration", () => {
-    let yellowcarposition = [-180, -60, 60, 180];
-    var random = yellowcarposition[(Math.floor(Math.random() * yellowcarposition.length))];
-    $yellowcar.css("left", random + "px");
+    let yellowcarposition = [-23, -8, 8, 23];
+    var random =
+      yellowcarposition[Math.floor(Math.random() * yellowcarposition.length)];
+    $yellowcar.css("left", random + "%");
     counter++;
-  })
+  });
 
-});
-
-
-$(() => {
-  const $redcar = $("#red-car");
-  const $bluecar = $("#blue-car");
+//keyboard left and right for red car
 
   $(document).keydown(function (e) {
     // Get the current position of the object
-    var currentPos = parseInt($('#red-car').css('left'));
- 
+    var currentPos = parseInt($("#red-car").css("left"));
+
     var minPos = -240;
     var maxPos = 240;
     // Check which arrow key was pressed
-    if (e.keyCode == 37 && currentPos > minPos) { // Left arrow key
+    if (e.keyCode == 37 && currentPos > minPos) {
+      // Left arrow key
       // Move the object left by 10 pixels
-      $('#red-car').css('left', currentPos - 20);
-    }
- 
-    else if (e.keyCode == 39 && currentPos < maxPos) { // Right arrow key
+      $("#red-car").css("left", currentPos - 20);
+    } else if (e.keyCode == 39 && currentPos < maxPos) {
+      // Right arrow key
       // Move the object right by 10 pixels
-      $('#red-car').css('left', currentPos + 20);
+      $("#red-car").css("left", currentPos + 20);
     }
   });
 
-});
 
-$(() => {
-  const $redcar = $("#red-car");
-  const $bluecar = $("#blue-car");
-  const $greencar = $("#green-car");
-  const $pinkcar = $("#pink-car");
-  const $yellowcar = $("#yellow-car");
+  //detection and scoring system
   let score = 0;
+  let highScores = [];
+
+  function resetGame() {
+    score = 0;
+    $("#score").text("Score: " + score);
+    $bluecar.css({ top: "0px", left: "-23%" });
+    $greencar.css({ top: "0px", left: "-8%" });
+    $pinkcar.css({ top: "0px", left: "8%" });
+    $yellowcar.css({ top: "0px", left: "23%" });
+    $("#message").text("");
+    const nameValue = $("#name").val();
+    $("#name").val(nameValue);
+  }
+
+  
+
 
   function detectCollision() {
+    if (!gameStarted) {
+      return;
+    }
     const redcarPos = $redcar.offset();
     const bluecarPos = $bluecar.offset();
     const greencarPos = $greencar.offset();
     const pinkcarPos = $pinkcar.offset();
-    const yellowcarPos = $yellowcar.offset();
+    const yellowcarPos = $yellowcar.offset();     
+
+
     if (
       redcarPos.left < bluecarPos.left + $bluecar.width() &&
       redcarPos.left + $redcar.width() > bluecarPos.left &&
       redcarPos.top < bluecarPos.top + $bluecar.height() &&
       redcarPos.top + $redcar.height() > bluecarPos.top
     ) {
-      alert(`Collision detected! Your score ${score}!! WELL DONE NOOB!`);
-      location.reload();
+
+  
+      const name = $("#name").val();
+      highScores.push({ name: name, score: score });
+      updateHighScores(highScores);
+      alert(`Game Over! Your score is ${score}. Well done, ${name}!`);
+      resetGame();
     }
     if (
       redcarPos.left < greencarPos.left + $greencar.width() &&
@@ -91,8 +128,12 @@ $(() => {
       redcarPos.top < greencarPos.top + $greencar.height() &&
       redcarPos.top + $redcar.height() > greencarPos.top
     ) {
-      alert(`Collision detected! Your score ${score}!! WELL DONE NOOB green !`);
-      location.reload();
+
+      const name = $("#name").val();
+      highScores.push({ name: name, score: score });
+      updateHighScores(highScores);
+      alert(`Game Over! Your score is ${score}. Well done, ${name}!`);
+      resetGame();
     }
     if (
       redcarPos.left < pinkcarPos.left + $pinkcar.width() &&
@@ -100,8 +141,14 @@ $(() => {
       redcarPos.top < pinkcarPos.top + $pinkcar.height() &&
       redcarPos.top + $redcar.height() > pinkcarPos.top
     ) {
-      alert(`Collision detected! Your score ${score}!! WELL DONE NOOB Pink killed u!`);
-      location.reload();
+
+        const name = $("#name").val();
+        highScores.push({ name: name, score: score });
+        updateHighScores(highScores);
+        alert(`Game Over! Your score is ${score}. Well done, ${name}!`);
+        resetGame();
+       
+     
     }
     if (
       redcarPos.left < yellowcarPos.left + $yellowcar.width() &&
@@ -109,15 +156,51 @@ $(() => {
       redcarPos.top < yellowcarPos.top + $yellowcar.height() &&
       redcarPos.top + $redcar.height() > yellowcarPos.top
     ) {
-      alert(`Collision detected! Your score ${score}!! WELL DONE NOOB Yellow killed u!`);
-      location.reload();
-    }
-    else if (bluecarPos.top > $redcar.height() && greencarPos.top > $redcar.height() && pinkcarPos.top > $redcar.height() && yellowcarPos.top > $redcar.height()) {
-      score++;
-      ($("#score").text("Score: " + score));
 
+      const name = $("#name").val();
+      highScores.push({ name: name, score: score });
+      updateHighScores(highScores);
+      alert(`Game Over! Your score is ${score}. Well done, ${name}!`);
+      resetGame();
+    } else if (
+      bluecarPos.top > $redcar.height() &&
+      greencarPos.top > $redcar.height() &&
+      pinkcarPos.top > $redcar.height() &&
+      yellowcarPos.top > $redcar.height()
+    ) {
+      score++;
+      $("#score").text("Score: " + score);
     }
+    
+   
   }
 
   setInterval(detectCollision, 50);
+  function updateHighScores(highScores) {
+    // Sort high scores in descending order
+    highScores.sort((a, b) => b.score - a.score);
+  
+    // Only keep top 5 high scores
+    highScores = highScores.slice(0, 5);
+  
+    // Clear the previous high scores from the element
+    $("#high-scores").empty();
+  
+    // Add a header for the high scores
+    const $h2 = $('<h2>');
+    $h2.text("High Scores");
+    $("#high-scores").append($h2);
+  
+    // Add a row for each high score
+    highScores.forEach((score, index) => {
+      const name1 = score.name;
+      const value = score.score;
+      const $keepScore = $('<p>');
+      $keepScore.attr("id", "keepscore");
+      $keepScore.text(`${name1}: ${value}`);
+      $("#high-scores").append($keepScore);
+    });
+  }
+
+
 });
